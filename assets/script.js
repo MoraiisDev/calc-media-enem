f93355c005d2b9ee9331d1a20406e9a9a52cc7fa
João Vitor <joao25072005@gmail.com>
2023-12-06 17:18:34 -0300

    Fixed bugs with pesos

--- assets/script.js
+++ assets/script.js
@@ -12,27 +12,34 @@ calcMedia.addEventListener("click", function(e) {
 
     e.preventDefault()
 
-    const notaRE = document.querySelector('#notare').value;
-    const pesoRE = document.querySelector('#pesore').value;
+    const notaRE = parseFloat(document.querySelector('#notare').value,10);
+    let pesoRE = parseInt(document.querySelector('#pesore').value,10);
 
-    const notaCN = document.querySelector('#notacn').value;
-    const pesoCN = document.querySelector('#pesocn').value;
+    const notaCN = parseFloat(document.querySelector('#notacn').value,10);
+    let pesoCN = parseInt(document.querySelector('#pesocn').value,10);
 
-    const notaCH = document.querySelector('#notach').value;
-    const pesoCH = document.querySelector('#pesoch').value;
+    const notaCH = parseFloat(document.querySelector('#notach').value,10);
+    let pesoCH = parseInt(document.querySelector('#pesoch').value,10);
 
-    const notaLC = document.querySelector('#notalc').value;
-    const pesoLC = document.querySelector('#pesolc').value;
+    const notaLC = parseFloat(document.querySelector('#notalc').value,10);
+    let pesoLC = parseInt(document.querySelector('#pesolc').value,10);
 
-    const notaMT = document.querySelector('#notamt').value;
-    const pesoMT = document.querySelector('#pesomt').value;
+    const notaMT = parseFloat(document.querySelector('#notamt').value,10);
+    let pesoMT = parseInt (document.querySelector('#pesomt').value,10);
 
-    const mediaGeral = (notaRE * pesoRE)/5 + (notaCN * pesoCN)/5 + (notaCH * pesoCH)/5 + (notaLC * pesoLC)/5 + (notaMT * pesoMT)/5
+    let mediaPeso = parseInt(pesoRE + pesoCN + pesoCH + pesoLC + pesoMT);
+
+    const mediaGeral = 
+    Math.round( (notaRE * pesoRE + notaCN * pesoCN + notaCH * pesoCH + notaLC * pesoLC + notaMT * pesoMT) / Math.trunc(mediaPeso) );
 
     if (pesoRE > 5 || pesoCN > 5 || pesoCH > 5 || pesoLC > 5 || pesoMT > 5) {
         document.getElementById("resultMessage").innerHTML = "O peso máximo é 5! Insira um valor menor";
     }
-
+    
+    else if (notaRE > 1000 || notaCN > 1000 || notaCH > 1000 || notaLC > 1000 || notaMT > 1000) {
+        document.getElementById("resultMessage").innerHTML = "A nota máxima é 1000! Insira um valor menor";
+    }
+    
     else if (mediaGeral < 600) {
         document.getElementById("resultMessage").innerHTML = "Sua média no ENEM de 2023 é de: " + mediaGeral + " pontos. No proximo ano será ainda melhor!";
     }
